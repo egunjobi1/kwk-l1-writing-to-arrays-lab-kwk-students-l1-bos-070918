e08ca0a5dca82bf315e1f6178e7405e41f8b62f7
Oluwapelumi Egunjobi <pelumitegunjobi@gmail.com>
2018-07-11 20:21:01 +0000

    automatically backed up by learn

--- shopping.rb
+++ shopping.rb
@@ -1,10 +1,10 @@
-shopping_array = {}
+
 shopping_array = ["24K magic CD", "crocs", "potato", "ACT prep book"]
 
 price = [15.98, 35, 0.8, 15]
 
 iterate = 0
-
+shopping_array = {}
 shopping_array.each do |x|
   shopping_array[x] = price[iterate]
   iterate += 1 
