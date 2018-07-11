b03f92727b7e30f8fe6f5ebe49c968ae9318a22a
Oluwapelumi Egunjobi <pelumitegunjobi@gmail.com>
2018-07-11 20:23:54 +0000

    automatically backed up by learn

--- amazon.shopping.rb
+++ amazon.shopping.rb
@@ -1,4 +1,4 @@
-shopping_array = [["24K Magic CD"=> 15.98], ["crocs" => 35], ["potato" => 0.8"]
+shopping_array = [["24K Magic CD"=> 15.98], ["crocs" => 35], ["potato" => 0.8]
 shopping_array.each do |indShopping_array|
   puts "#{indShopping_array}"
 end
