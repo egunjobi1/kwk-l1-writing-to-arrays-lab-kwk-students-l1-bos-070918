8be030c65377f92aa0504bcd9b82205863833cd7
Oluwapelumi Egunjobi <pelumitegunjobi@gmail.com>
2018-07-11 20:10:51 +0000

    automatically backed up by learn

--- shopping.rb
+++ shopping.rb
@@ -1,6 +1,6 @@
-shopping_array = ["Natalie", "Cece", "Pelumi", "Sidney", "Hope", "Christina","Ella"]
+shopping_array = ["24K magic CD", "crocs", "potato", "ACT prep book"]
 
-price = ["15.98", "35", "0.8", "15", "Romme Strigid", "Billie Eilish", "Rihanna"]
+price = ["15.98", "35", "0.8", "15"]
 
 iterate = 0
 
