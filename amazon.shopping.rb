a6fd73194349cb455dbf131388e505d91da86dc4
Oluwapelumi Egunjobi <pelumitegunjobi@gmail.com>
2018-07-11 20:17:26 +0000

    automatically backed up by learn

--- amazon.shopping.rb
+++ amazon.shopping.rb
@@ -1,4 +1,4 @@
-shopping_array = [["24K Magic CD","15.98"], ["crocs","35"], ["potato", "0.8"]].reverse
-sandwiches.each do |indSandwich|
+shopping_array = [["24K Magic CD","15.98"], ["crocs","35"], ["potato", "0.8"]]
+shopping_array.each do |indSandwich|
   puts "#{indSandwich}"
 end
