029694fb57bba4fb05561fdfb2c81d0714e039d2
Oluwapelumi Egunjobi <pelumitegunjobi@gmail.com>
2018-07-11 20:17:47 +0000

    automatically backed up by learn

--- amazon.shopping.rb
+++ amazon.shopping.rb
@@ -1,4 +1,4 @@
 shopping_array = [["24K Magic CD","15.98"], ["crocs","35"], ["potato", "0.8"]]
-shopping_array.each do |indSandwich|
-  puts "#{indSandwich}"
+shopping_array.each do |indShopping_array|
+  puts "#{indShopping_arrayh}"
 end
